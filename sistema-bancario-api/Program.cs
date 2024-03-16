3fd2c21f40e8957309ccd08d64bacc2cd2e5c036
Luis Ubeda <luisubeda.90@outlook.com>
2024-03-15 22:22:25 -0600

    Adicion de clase usuario
    Se agrego clase usuario y entity framework

--- sistema-bancario-api/Program.cs
+++ sistema-bancario-api/Program.cs
@@ -1,11 +1,18 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using sistema_bancario_post.Data;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<UserLoginTable>(options =>
+{
+    options.UseOracle(builder.Configuration.GetConnectionString("UserLoginTable"));
+}
+);
 
 var app = builder.Build();
 
@@ -23,4 +30,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
