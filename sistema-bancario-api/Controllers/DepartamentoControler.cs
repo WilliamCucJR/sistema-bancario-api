469ae03f0433b3661592451b276aa0b497b72e23
Luis Ubeda <luisubeda.90@outlook.com>
2024-04-17 21:51:05 -0600

    Departamento
    creacion tabla deptos y controller todos y por id

---
+++ sistema-bancario-api/Controllers/DepartamentoControler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using sistema_bancario_api.Data;
+using sistema_bancario_api.Data.Entities.Table;
+
+namespace sistema_bancario_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentoControler : ControllerBase
+    {
+        private readonly DepartamentoTable deptotable;
+
+        public DepartamentoControler(DepartamentoTable deptotable)
+        {
+            this.deptotable = deptotable;
+        }
+
+        // GET: api/Movimientos
+        [HttpGet("GetAllDepartamentos")]
+        public async Task<ActionResult<IEnumerable<DEPARTAMENTO>>> GetDepartamentos()
+        {
+            return await deptotable.Deptos.FromSqlRaw("SELECT * FROM DEPARTAMENTO ORDER BY ID").ToListAsync();
+        }
+
+        // GET: api/Movimientos/5
+        [HttpGet("GetDepartamento/{id}")]
+        public async Task<ActionResult<DEPARTAMENTO>> GetDepartamento(int id)
+        {
+            var dep = await deptotable.Deptos.FromSqlRaw($"SELECT * FROM DEPARTAMENTO WHERE ID = {id}").FirstOrDefaultAsync();
+
+            if (dep == null)
+            {
+                return NotFound();
+            }
+
+            return dep;
+        }
+    }
+}
---
+++ sistema-bancario-api/Data/Entities/Table/DEPARTAMENTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace sistema_bancario_api.Data.Entities.Table
+{
+    [Table("MUNICIPIO")]
+    public class DEPARTAMENTO
+    {
+        [Key]
+        public int ID { get; set; }
+        public string? NOMBRE_DEL_DEPARTAMENTO {  get; set; }
+
+    }
+}
